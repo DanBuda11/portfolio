dedbee5143586188b33d38d24890d05497a42e60
Dan Buda <danbuda@gmail.com>
2019-05-27 11:40:57 -0500

    content typo, add moon icon to toggle button, fix github link icons

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,19 @@ const items = document.querySelectorAll(
   'body, a, #header, #about, #major, #minor, #skills, #footer, .themeBtn, .section__title, .about__name, .tech'
 );
 const itemsArray = Array.from(items);
-const btn = document.querySelector('.themeBtn');
+const btn = document.querySelector('.theme__btn');
+const icon = document.querySelector('.theme__icon');
 
 btn.addEventListener('click', () => {
   itemsArray.forEach(item => {
     item.classList.toggle('light');
   });
+
+  if (icon.classList.contains('fa-sun')) {
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+  } else {
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  }
 });
