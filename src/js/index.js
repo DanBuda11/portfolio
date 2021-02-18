3a2a4066f25d4256eaf304dce3b05a117fe3d41a
Dan Buda <danbuda@gmail.com>
2021-02-18 14:39:24 -0600

    finish professional work section

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 const items = document.querySelectorAll(
-  'body, a, #header, #about, #major, #minor, #skills, #footer, .theme__btn, .section__title, .about__name, .tech'
+  'body, a, #header, #about, #professional, #major, #minor, #skills, #footer, .theme__btn, .section__title, .about__name, .tech'
 );
 const itemsArray = Array.from(items);
 const btn = document.querySelector('.theme__btn');
